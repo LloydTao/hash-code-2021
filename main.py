54da854cdad8ba1b3a117e36a509e207cfa9ae93
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2021-02-25 18:22:55 +0000

    Create Python template

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+import numpy as np
+
+
+class Solution:
+    def solve_stuff(a, b, c, n):
+        pass
+
+
+if __name__ == "__main__":
+    pass
