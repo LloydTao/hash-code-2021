4750582c5ccbb27a434e3545c87f44a043eb09d5
Matt Drayton <matthewdrayton@outlook.com>
2021-02-25 19:04:05 +0000

    Add string outputter to schedule

--- schedule.py
+++ schedule.py
@@ -1,3 +1,6 @@
+from street import Street
+from intersection import Intersection
+
 class Schedule:
     
     def __init__(self, intersection, street_schedules):
@@ -15,3 +18,30 @@ class Schedule:
         return f"<Schedule for intersection {self.intersection.id_}>"
     
 
+    def output_format(self):
+        """Returns the schedule in the file format
+
+        Returns:
+            string: String representation of the schedule to be combined into output
+        """
+        output = f"{self.intersection.id_}\n"
+        output += f"{len(self.street_schedules)}\n"
+
+        for schedule in self.street_schedules:
+            street = schedule[0]
+            time_green = schedule[1]
+
+            output += f"{street.name} {time_green}\n"
+
+        return output
+
+
+if __name__ == "__main__":
+    intersection = Intersection(0)
+    intersection2 = Intersection(1)
+
+    street = Street(0, 1, "Main Road", 5)
+
+    schedule = Schedule(intersection, [(street, 5)])
+
+    print(schedule.output_to_file())
